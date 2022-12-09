8e147e65f9a3671befffe6842b6b28b3f9d0cb62
Sam B <reposme@outlook.com>
2022-12-09 21:33:56 +0000

    Added variable using moment.js and appended it to the currentDay ID in index.html

---
+++ starter/script.js
@@ -0,0 +1,16 @@
+// var hello = "hello";
+// console.log(hello);
+
+var currentDay = moment().format('dddd, MMMM Do');
+
+console.log(currentDay);
+
+// append today's date to <p id="currentDay"
+
+$( "#currentDay" ).append(currentDay);
+
+// create container using bootstrap & js
+
+
+
+
